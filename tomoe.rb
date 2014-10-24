3dc48e67fc44afd7a85b6f45e353439ea013b408
Kazuhito Hokamura <k.hokamura@gmail.com>
2014-10-24 18:44:19 +0900

    Update sha1

--- tomoe.rb
+++ tomoe.rb
@@ -4,7 +4,7 @@ class Tomoe < Formula
   homepage "https://github.com/hokaccha/tomoe"
   version 'v0.1.0'
   url "https://github.com/hokaccha/tomoe/releases/download/v0.0.1/tomoe_0.0.1_darwin_amd64.zip"
-  sha1 "f2f49c11a45918644bc3022196955a15fd2a06be"
+  sha1 "47e8ca95264efb363959ae6b3518d231bd9d2f71"
 
   def install
     bin.install 'tomoe'
